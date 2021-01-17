fc347c9046a21827dd386f6795415ff8c0cb2992
Bri8102 <damioniru16@gmail.com>
2021-01-17 20:53:23 +0100

    created component for destination show page

---
+++ src/components/DestinationShow.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Card, CardBody, CardTitle, CardImg, Button } from 'reactstrap';
+import Activities from '../containers/Activities';
+import { deleteDestination, toggleVisited, toggleBucketList } from '../actions/destinationActions';
+
+
+const DestinationShow = props => {
+
+  let destination = props.destinations.filter(destination => destination.id == props.match.params.id)[0];
+
+  const handleDelete = () => {
+    props.deleteDestination(destination.id);
+    const path = `/destinations`;
+    props.history.push(path);
+  };
+
+  const handleVisited = () => {
+    props.toggleVisited(destination, destination.id)
+  }
+
+  const handleBucketList = () => {
+    props.toggleBucketList(destination, destination.id)
+  }
+
+  return (
+    <div>
+      <Card className = 'DestinationShow'>
+      <CardBody>
+          <CardTitle >{destination && destination.name} - {destination && destination.country}</CardTitle>
+          <CardImg className='DestinationImage' src={destination && destination.image} alt={destination && destination.name} /><br></br><br></br>
+        </CardBody>
+        <Button onClick={handleVisited} className='VisitedButton' style={{alignItems: 'center' }}>
+          {destination && destination.visited === false ? 'Add to Visited' : 'You Have Visited'}
+        </Button>
+        <Button onClick={handleBucketList} className='BucketListButton'>
+          {destination && destination.bucket_list === false ? 'Add to Bucket List' : 'This Is In your Bucket List'}
+        </Button>
+        <Button onClick={handleDelete} className='DeleteButton'>Delete This Destination</Button>
+      </Card>
+
+      <Activities destination={destination} />
+    </div>
+  );
+};
+
+export default connect(null, { deleteDestination, toggleVisited, toggleBucketList })(DestinationShow);
