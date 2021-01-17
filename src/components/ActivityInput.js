b06fc457c8846e68f3600e37c051749666df7988
Bri8102 <damioniru16@gmail.com>
2021-01-17 20:49:48 +0100

    created activity form component

---
+++ src/components/ActivityInput.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { addActivity } from '../actions/activityActions';
+
+class ActivityInput extends Component {
+
+  state = {
+    title: ''
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addActivity(this.state, this.props.destination.id);
+    this.setState({
+      title: ''
+    });
+  };
+
+  render() {
+    return (
+      <div className='ActivityForm'>
+        <Form onSubmit={this.handleSubmit}>
+          
+            <h5>New Activity</h5>
+          
+
+          <FormGroup row>
+            <Label for='title' sm={2}>Things To Do:</Label>
+            <Col md={10}>
+              <Input type='text' name='title' value={this.state.title} placeholder='Beach, City Tours, Good Eating, Museums . . .' onChange={this.handleChange}/>
+            </Col>
+          </FormGroup>
+
+          <Button>Add Activity</Button>
+        </Form>
+      </div>
+    );
+  }
+};
+
+export default connect(null, { addActivity })(ActivityInput);
