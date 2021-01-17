5acb655856cc0f3419e9574e9b3fe98dc13a6662
Bri8102 <damioniru16@gmail.com>
2021-01-17 20:00:53 +0100

    created home page

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="HomePage">
+            <h1>Bon Voyage</h1><br/><br/>
+            <h3>Close your eyes and imagine you're there...</h3>
+        </div>
+    )
+}
+
+export default Home;
