aa2cac9d329d07666c60d86e61b2fc02ec3151a9
Bri8102 <damioniru16@gmail.com>
2021-01-17 20:42:58 +0100

    Destination List routes and page Stlying

---
+++ src/components/DestinationList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
+
+const DestinationsList = props => {
+
+  return (
+    <div className='DestinationContainer'>
+      <Link to='/destinations/new' className='NewDestinationBtn' role='button'>Add Destination</Link>
+
+    <h3 style={{color: 'white', fontWeight: '700', margin: '20px' }}>My Destinations</h3>
+      {props.destinations.map(destination =>
+        <Link key={destination.id} to={`/destinations/${destination.id}`}>
+          <Card className = 'DestinationCard'>
+            <CardBody>
+              <CardTitle>{destination.name} - {destination.country}</CardTitle>
+              <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
+            </CardBody>
+          </Card>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default DestinationsList;
